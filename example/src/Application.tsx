1b4168d62a88dd5b8e074843e3a2563ffabb10fa
Felipe Valadares <felipe.valadares2@gmail.com>
2022-06-07 13:54:59 -0300

    simplify example app

--- example/src/Application.tsx
+++ example/src/Application.tsx
@@ -1,7 +1,7 @@
 import Nullstack from "nullstack";
 import "./Application.scss";
 import { App } from "@capacitor/app";
-import { PointSDK, PointEnvironment, QueryType, Goal, SpecificGoal, WorkoutRatings } from "../../dist/esm";
+import { PointSDK, PointEnvironment, QueryType, Goal } from "../../dist/esm";
 
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdhbml6YXRpb24iOiJUZXN0aW5nIE9yZyIsIm9yZ0lkIjozNSwic3ViIjoiNjI4NjkwNmU3NDk0MWUwMDZlNjE3ZGJjIiwiaWF0IjoxNjU0MTc4MzkzLCJleHAiOjE2NTQyNjQ3OTN9.eNho00BVktk1onUUs7KlHJG9Jcifx5ejzEFvF0Byxjw";
@@ -35,29 +35,13 @@ class Application extends Nullstack {
       <main>
         <button onclick={this.requestPermissions}>Request permissions</button>
         <br></br>
-        <button onclick={this.setUserToken}>Set user token</button>
-        <br></br>
-        <button onclick={this.enableBackgroundDelivery}>Enable Background listeners</button>
-        <br></br>
-        <button onclick={this.setupBackgroundQueries}>Setup Background Queries</button>
-        <br></br>
-        <button onclick={this.setupStepCountBackgroundQuery}>Setup Background for stepCount</button>
-        <br></br>
-        <button onclick={this.enableAllForegroundListeners}>Enable foreground listeners</button>
-        <br></br>
-        <button onclick={this.stopAllForegroundListeners}>Stop foreground listeners</button>
-        <br></br>
         <button onclick={this.getUserData}>Get user data</button>
         <br></br>
         <button onclick={this.setUserGoal}>Set user goal Athletic Performance</button>
         <br></br>
-        <button onclick={this.setUserGoalWeight}>Set user goal Weight Loss</button>
-        <br></br>
         <button onclick={this.getUserWorkouts}>Get user workouts</button>
         <br></br>
         <button onclick={this.getUserTrends}>Get user trends</button>
-        <br></br>
-        <button onclick={this.getUserWorkoutById}>Get workout by id</button>
         <br></br>
         <button onclick={this.getUserDailyHistory}>Get daily history</button>
         <br></br>
@@ -95,22 +79,6 @@ class Application extends Nullstack {
     await PointSDK.enableAllBackgroundDelivery();
   }
 
-  async setupStepCountBackgroundQuery() {
-    await PointSDK.setupBackgroundQueryForType({ type: QueryType.StepCount });
-  }
-
-  async enableStepCountBackgroundDelivery() {
-    await PointSDK.enableBackgroundDeliveryForType({ type: QueryType.StepCount });
-  }
-
-  async disableStepCountBackgroundDelivery() {
-    await PointSDK.disableBackgroundDeliveryForType({ type: QueryType.StepCount });
-  }
-
-  async disableAllBackgroundDelivery() {
-    await PointSDK.disableAllBackgroundDelivery();
-  }
-
   async enableAllForegroundListeners() {
     await PointSDK.enableAllForegroundListeners();
   }
@@ -130,17 +98,6 @@ class Application extends Nullstack {
 
   async getUserWorkouts() {
     const result = await PointSDK.getUserWorkouts({ offset: 0 });
-    Application.logAndAlert(result);
-  }
-
-  async getUserWorkoutById() {
-    const result = await PointSDK.getUserWorkoutById({ workoutId: 5793 });
-    Application.logAndAlert(result);
-  }
-
-  async rateWorkout() {
-    const ratings = { difficulty: 4, energy: 5, instructor: 3 };
-    const result = await PointSDK.rateWorkout({ id: 5793, ratings });
     Application.logAndAlert(result);
   }
 
@@ -171,16 +128,6 @@ class Application extends Nullstack {
 
   async setUserGoal() {
     const result = await PointSDK.setUserGoal({ goal: Goal.AthleticPerformance });
-    Application.logAndAlert(result);
-  }
-
-  async setUserSpecificGoal() {
-    const result = await PointSDK.setUserSpecificGoal({ specificGoal: SpecificGoal.BuildLeanMuscle });
-    Application.logAndAlert(result);
-  }
-
-  async setUserGoalWeight() {
-    const result = await PointSDK.setUserGoal({ goal: Goal.WeightLoss });
     Application.logAndAlert(result);
   }
 
