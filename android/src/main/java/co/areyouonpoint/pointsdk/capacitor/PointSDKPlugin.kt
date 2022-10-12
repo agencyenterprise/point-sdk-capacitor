1ed1b7ecfece3f00228e70632a8abd132b94a064
Felipe Valadares <felipe.valadares2@gmail.com>
2022-10-12 16:15:36 -0300

    - Adding setup, setUserToken and logout methods - Update native sdk version

--- android/src/main/java/co/areyouonpoint/pointsdk/capacitor/PointSDKPlugin.kt
+++ android/src/main/java/co/areyouonpoint/pointsdk/capacitor/PointSDKPlugin.kt
@@ -1,5 +1,9 @@
 package co.areyouonpoint.pointsdk.capacitor
 
+import co.areyouonpoint.pointsdk.PointClient
+import co.areyouonpoint.pointsdk.core.logger.LogLevel
+import co.areyouonpoint.pointsdk.domain.PointEnvironment
+import co.areyouonpoint.pointsdk.domain.exceptions.PointException
 import com.getcapacitor.JSObject
 import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
@@ -8,13 +12,50 @@ import com.getcapacitor.annotation.CapacitorPlugin
 
 @CapacitorPlugin(name = "PointSDK")
 class PointSDKPlugin : Plugin() {
-    private val implementation = PointSDK()
+    private var pointClient: PointClient? = null
+
+    @PluginMethod
+    fun setup(call: PluginCall) {
+        val verbose = call.getBoolean("verbose") ?: false
+
+        pointClient = PointClient.getInstance(
+            context = context,
+            clientId = call.getString("clientId")!!,
+            clientSecret = call.getString("clientSecret")!!,
+            apiEnvironment = environmentsMapping(call.getString("environment")),
+            logLevel = if (verbose) LogLevel.DEBUG else LogLevel.NONE
+        )
+
+        call.resolve()
+    }
 
     @PluginMethod
     fun setUserToken(call: PluginCall) {
-        val value = call.getString("userToken") ?: "default value"
-        val ret = JSObject()
-        ret.put("value", implementation.setUserToken(value))
-        call.resolve(ret)
+        try {
+            pointClient?.setUserToken(call.getString("userToken")!!)
+            call.resolve()
+        } catch (ex: PointException) {
+            call.reject(ex.message)
+        }
+    }
+
+    @PluginMethod
+    fun logout(call: PluginCall) {
+        try {
+            pointClient?.logout()
+            call.resolve()
+        } catch (ex: PointException) {
+            call.reject(ex.message)
+        }
+    }
+
+    private fun environmentsMapping(env: String?): PointEnvironment {
+        return when(env) {
+            "development" -> PointEnvironment.DEVELOPMENT
+            "staging" -> PointEnvironment.STAGING
+            "production" -> PointEnvironment.PRODUCTION
+            "preprod" -> PointEnvironment.PRE_PROD
+            else -> PointEnvironment.DEVELOPMENT //DEVELOPMENT IS THE DEFAULT SAME AS iOS
+        }
     }
 }
